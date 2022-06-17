70354999040d06d6048f2f1b2ad5d159a0f96b3f
Scott Shawcroft <scott@tannewt.org>
2022-06-17 16:51:04 -0700

    initial seattle census blocks

---
+++ seattle_census_blocks/prep_data.py
@@ -0,0 +1,31 @@
+# This script was used to filter the blocks from the block20 redistricting data (too big for github)
+# for washington to seattle. Seattle was defined by existing districts plus a polygon to bridge the
+# ship canal.
+
+import geopandas
+import pathlib
+
+existing_districts = geopandas.read_file("Seattle_City_Council_Districts___sccdst_area/modified.shp")
+
+census_data = geopandas.read_file("block20/wa_2020_pl94171_block.dbf")
+print(type(census_data))
+print(census_data)
+
+blocks = geopandas.read_file("block20/block20.shp")
+print(type(blocks))
+print(blocks)
+
+del census_data["geometry"]
+merged = blocks.merge(census_data, on="GEOID20")
+print(type(merged))
+print(merged)
+
+existing_districts = existing_districts.to_crs(blocks.crs)
+
+filtered = merged.clip(existing_districts)
+print(type(filtered))
+print(filtered)
+
+filtered.plot().get_figure().savefig("seattle.png")
+
+filtered.to_file("seattle_blocks.shp")
