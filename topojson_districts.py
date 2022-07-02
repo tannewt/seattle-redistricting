56540b6d366721af7fc52317e6fa6f0fcfa3dc3c
Scott Shawcroft <scott@tannewt.org>
2022-07-01 22:13:36 -0700

    crs

--- topojson_districts.py
+++ topojson_districts.py
@@ -10,5 +10,6 @@ class TopoJSONReport:
         blocks["GEOID20"] = pandas.to_numeric(blocks["GEOID20"], errors='coerce').convert_dtypes()
         district_bounds = blocks.merge(plan, on="GEOID20")
         district_bounds = district_bounds.dissolve(by="District")
+        district_bounds = district_bounds.to_crs("urn:ogc:def:crs:OGC:1.3:CRS84")
         json = district_bounds.to_json(show_bbox=True)
         return ("Districts", "```geojson\n" + json + "\n```")
