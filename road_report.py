2ebf082d6d73fe64c28585e29474c82e715eaf60
Scott Shawcroft <scott@tannewt.org>
2022-07-05 22:04:03 -0700

    subgraph render

--- road_report.py
+++ road_report.py
@@ -7,15 +7,16 @@ import pandas
 import sys
 import osmnx
 import networkx
+import matplotlib.pyplot as plt
 
 class RoadReport:
 
     def __init__(self):
-        self.roads = osmnx.load_graphml("sources/roads.graphml")
-        self.roads = self.roads.subgraph(max(networkx.strongly_connected_components(self.roads), key=len))
-        self.nodes = osmnx.graph_to_gdfs(self.roads, edges=False)
         blocks = geopandas.read_file("seattle_census_blocks/seattle_blocks.shp")
-        self.nodes = self.nodes.to_crs(blocks.crs)
+        self.roads = osmnx.load_graphml("sources/roads.graphml")
+        self.roads = osmnx.project_graph(self.roads, blocks.crs)
+        self.roads = self.roads.subgraph(max(networkx.weakly_connected_components(self.roads), key=len))
+        self.nodes = osmnx.graph_to_gdfs(self.roads, edges=False)
         blocks = blocks[["GEOID20", "geometry"]]
         blocks["GEOID20"] = pandas.to_numeric(blocks["GEOID20"], errors='coerce').convert_dtypes()
         self.blocks = blocks[["GEOID20", "geometry"]]
@@ -27,8 +28,17 @@ class RoadReport:
 
         joined = blocks.dissolve(by="District")
         for bounds in joined["geometry"]:
-            intersecting_nodes = self.nodes[self.nodes.intersects(bounds.buffer(50))].index
+            intersecting_nodes = self.nodes[self.nodes.intersects(bounds.buffer(10))].index
             district_roads = self.roads.subgraph(intersecting_nodes)
-            for sg in networkx.strongly_connected_components(district_roads):
+            count = networkx.number_weakly_connected_components(district_roads)
+            print(count)
+            colors = osmnx.plot.get_colors(count)
+            for i, sg in enumerate(networkx.weakly_connected_components(district_roads)):
                 print(len(sg))
+                for node in sg:
+                    district_roads.nodes[node]["partition_size"] = i
+            nc = osmnx.plot.get_node_colors_by_attr(district_roads, attr="partition_size")
+            ax = joined[joined["geometry"]==bounds].boundary.plot(edgecolor="black")
+            fig, new_ax = osmnx.plot_graph(district_roads, ax=ax, node_color=nc)
+
             print()
